613461768a4ad7b59324b3fc483c23af908cb1fa
Alexey Medov <amedov@gitlab.int>
2026-01-27 22:31:27 +0300

    fix description

--- selectel/Lesson_1/variables.tf
+++ selectel/Lesson_1/variables.tf
@@ -59,6 +59,5 @@ variable "openstack_url" {
 # 
 
 resource "random_password" "my_serviceuser_1_password" {
-  length = 20
-  description = "Сгенерируем случайный пароль для нашего будущего сервисного пользователя my_serviceuser_1"
+  length = 20 # "Сгенерируем случайный пароль для нашего будущего сервисного пользователя my_serviceuser_1 длинной в 20 символов"
 }
