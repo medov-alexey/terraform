3a543c7216556eb21c95004b2eb8f340964da1b7
Alexey Medov <amedov@gitlab.int>
2026-01-28 13:46:52 +0300

    small fix

--- selectel/Lesson_1/output.tf
+++ selectel/Lesson_1/output.tf
@@ -1,4 +1,4 @@
-output "db_password" {
+output "my_serviceuser_1_password" {
   value     = random_password.my_serviceuser_1_password.result
   sensitive = true
   description = "Пароль для создаваемого сервисного пользователя my_serviceuser_1 (используйте: terraform output -raw my_serviceuser_1_password)"
