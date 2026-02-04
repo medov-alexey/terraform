fff77f9c51e969e26b1d28b1be0f061a505bc6eb
Alexey Medov <amedov@gitlab.int>
2026-02-04 14:05:00 +0300

    final fix for lesson - 4

--- selectel/Lesson_4/README.md
+++ selectel/Lesson_4/README.md
@@ -1,9 +1,7 @@
 
-Еще не готово, надо пройти этот путь и убрать этот комментарий <---- (пока просто добавил в гит чтобы потом не начинать с нуля)
-
-Lesson 4 - из под основного сервисного пользователя, в существующем проекте my_project, создаем облачный сервер my_test_server_1, произвольной конфигурации
-           с загрузочным сетевым диском и дополнительным сетевым диском. Это включает в себя создание таких openstack сущностей:
-           flavor, keypair, network (private), subnet for network (also for private), external network, router, router interface, port, volume 1, volume 2, server, floatingip_1
+Lesson 4 - из под основного сервисного пользователя, в существующем проекте my_project, создаем облачный сервер server_1, произвольной конфигурации
+           с загрузочным сетевым диском и дополнительным сетевым диском. Это включает в себя создание и/или использование таких openstack сущностей:
+           flavor, keypair, network (private), subnet for private network, external network, router, router interface, port, volume 1, volume 2, server, floatingip_1
 
 1. terraform init
 
--- selectel/Lesson_4/main.tf
+++ selectel/Lesson_4/main.tf
@@ -17,16 +17,15 @@ resource "openstack_compute_flavor_v2" "tiny" {
   lifecycle {
     create_before_destroy = true
   }
-
 }
 
-# создаем приватную сеть 
+# создаем приватную сеть (по сути это локальная сеть внутри облака, можем в этой сети объединять наши виртуалки как будто они в одной сети)
 resource "openstack_networking_network_v2" "my_private_network" {
   name           = "my_private_network"
   admin_state_up = "true"
 }
 
-# создаем подсеть в нашей приватной сети которую создали выше 
+# создаем подсеть в нашей приватной сети (и из этой подсети наши сервера будут получать ip адреса)
 resource "openstack_networking_subnet_v2" "subnet_for_my_private_network" {
   name       = "subnet_for_my_private_network"
   network_id = openstack_networking_network_v2.my_private_network.id
@@ -35,16 +34,61 @@ resource "openstack_networking_subnet_v2" "subnet_for_my_private_network" {
 
 #-------------------------------------------------------------------------------------
 
-# получаем информацию о существующей внешней сети
+# отправляем запрос к API облака, с целью получить все сети в моем проекте и отфильтровать только те, у которых флаг external равен true".
+# если такая сеть найдена, её параметры (ID, имя и т.д.) сохраняются в этом data-блоке.
+#
+# Еще один вариант объяснения:
+#
+# Блок data "openstack_networking_network_v2" "my_external_network" — это поисковый запрос, который находит в вашем облачном проекте предустановленную провайдером публичную сеть.
+# Его основная цель в примере — предоставить информацию (ID или имя этой сети) для другого ресурса, который создает и назначает публичный IP-адрес вашему серверу.
+#
+# Вот пример того что получаем от облака:
+#
+# data.openstack_networking_network_v2.my_external_network
+# {
+#   "admin_state_up" = "true"
+#   "all_tags" = toset([])
+#   "availability_zone_hints" = tolist([])
+#   "description" = ""
+#   "dns_domain" = ""
+#   "external" = true
+#   "id" = "f16140a4-e736-4655-9a1b-d7d2df6f969e" <----------------- id внешней сети (у которой есть доступ в интернет)
+#   "matching_subnet_cidr" = tostring(null)
+#   "mtu" = 1500
+#   "name" = "external-network"                   <----------------- обрати внимание на этом имя (оно потом будет использоваться для получения внешнего ip адреса из этой сети ниже в коде)
+#   "network_id" = tostring(null)
+#   "region" = "ru-9"
+#   "segments" = toset([])
+#   "shared" = "false"
+#   "status" = tostring(null)
+#   "subnets" = tolist([
+#     "0a70ca1b-6613-49a3-9e65-a89d484330f7",
+#     "0fe7a122-c205-4ece-ba0f-94e4497d955f",
+#     "1751014e-9207-40a3-ac7a-7763dd0029a4",
+#     "23a85638-428b-44f0-a4e0-d2fbb48f0093",
+#     "2afae582-6c48-4d59-981e-f499fb61b1ea",
+#     "2b6e3274-08cc-46ea-89ad-672fee9122ae",
+#     "30ab21be-e7fe-4ae1-a381-0426e4a6e0b0",
+#     "e945f98d-99bf-41b6-9bc2-c9e82ee3789b",
+#     "ebece733-c29f-463c-ae07-711e548edf62",
+#   ])
+#   "tags" = toset(null) /* of string */
+#   "tenant_id" = "5e204c6ffe184b9f8b69344d36178244"
+#   "transparent_vlan" = false
+# }
 data "openstack_networking_network_v2" "my_external_network" {
   external = true
 }
+
+
 
 # создаем роутер и указываем id внешней сети (у которой есть доступ в интернет)
 resource "openstack_networking_router_v2" "my_router" {
   name                = "my_router"
   external_network_id = data.openstack_networking_network_v2.my_external_network.id
 }
+
+
 
 # присоединяем роутер к приватной подсети (позволяем трафику ходить между сетями приватной и внешней)
 resource "openstack_networking_router_interface_v2" "my_router_interface_1" {
@@ -64,7 +108,58 @@ resource "openstack_networking_port_v2" "my_port" {
 
 #-------------------------------------------------------------------------------------
 
-# получаем данные об образе
+# получаем данные об образе из API облака
+# 
+# вот пример данных которые получаем:
+#
+# data.openstack_images_image_v2.image_1
+# {
+#   "checksum" = "97d4e186acf08652845014cc8ceda444"
+#   "container_format" = "bare"
+#   "created_at" = "2026-02-03T06:42:22Z"
+#   "disk_format" = "raw"
+#   "file" = "/v2/images/deaffa57-a54a-4018-a818-f56ae92f7cec/file"
+#   "hidden" = false
+#   "id" = "deaffa57-a54a-4018-a818-f56ae92f7cec"
+#   "member_status" = tostring(null)
+#   "metadata" = tomap({})
+#   "min_disk_gb" = 5
+#   "min_ram_mb" = 512
+#   "most_recent" = true
+#   "name" = "Ubuntu 20.04 LTS 64-bit"
+#   "name_regex" = tostring(null)
+#   "owner" = "3acf7ceddc024b86b86ef151e4972805"
+#   "properties" = tomap({
+#     "direct_url" = "rbd://49f37ff2-48ad-4eb7-962e-4e6365bc9c03/images/deaffa57-a54a-4018-a818-f56ae92f7cec/snap"
+#     "hw_disk_bus" = "scsi"
+#     "hw_qemu_guest_agent" = "yes"
+#     "hw_scsi_model" = "virtio-scsi"
+#     "os_distro" = "ubuntu"
+#     "os_hash_algo" = "sha512"
+#     "os_hash_value" = "1e1d7fa0ced3b190f5ad0150fb3e60a765d51301082ff61f7e13b4af9f97bd7703f0d942bc3d713135354c953c5142f6f4ea748fa248a9a6343c8ecf06b0a1c5"
+#     "os_type" = "linux"
+#     "stores" = "ru-9a"
+#     "watchdog" = "pause"
+#     "x_sel_image_agent_type" = "cloud-init"
+#     "x_sel_image_os_arch" = "amd64"
+#     "x_sel_image_os_dist" = "ubuntu"
+#     "x_sel_image_owner" = "Selectel"
+#     "x_sel_image_source_file" = "ubuntu-focal-amd64-selectel-master-product-0.1.img"
+#     "x_sel_image_type" = "master"
+#     "x_sel_os_type" = "linux"
+#   })
+#   "protected" = false
+#   "region" = "ru-9"
+#   "schema" = "/v2/schemas/image"
+#   "size_bytes" = 4001955840
+#   "size_max" = tonumber(null)
+#   "size_min" = tonumber(null)
+#   "sort" = "name:asc"
+#   "tag" = tostring(null)
+#   "tags" = toset([])
+#   "updated_at" = "2026-02-03T06:45:51Z"
+#   "visibility" = "public"
+# }
 data "openstack_images_image_v2" "image_1" {
   name        = "Ubuntu 20.04 LTS 64-bit"
   most_recent = true
@@ -131,18 +226,25 @@ resource "openstack_compute_instance_v2" "server_1" {
   }
 }
 
-# запрашиваем плавающий (внешний) IP-адрес из пула внешней сети
+# запрашиваем плавающий (внешний) IP-адрес из пула внешней сети с именем external-network
 resource "openstack_networking_floatingip_v2" "my_floatingip" {
   pool = "external-network"
 }
 
-# связываем плавающий (внешний) IP с сетевым портом сервера на котором уже висит ip из внутренней сети
+# связываем плавающий (внешний) IP, с сетевым портом сервера на котором уже висит ip из внутренней сети
+# изнутри виртуальной машины будет виден только ip адрес из приватной сети, а внешний ip адрес виден не будет
+# это так работает, так как в данном облаке реализован NAT один к одному
+#
+# NAT 1:1 — это полное зеркалирование всех портов внешнего IP-адреса на все порты внутреннего IP-адреса вашего сервера,
+# дающее ему прямое присутствие в интернете под постоянным белым адресом. Всё, что приходит на белый IP, автоматически пересылается на сервер 1:1 без изменений портов.
+#
+# В OpenStack не всегда используется NAT 1:1, но для прямого доступа к виртуальным машинам извне (Floating IP) - это основной механизм
 resource "openstack_networking_floatingip_associate_v2" "association_1" {
   port_id     = openstack_networking_port_v2.my_port.id
   floating_ip = openstack_networking_floatingip_v2.my_floatingip.address
 }
 
-# выводим публичный IP-адрес созданного сервера
-output "public_ip_address" {
-  value = openstack_networking_floatingip_v2.my_floatingip.fixed_ip
+# выводим публичный IP-адрес созданного нами сервера
+output "server_ip_address" {
+  value = openstack_networking_floatingip_v2.my_floatingip.address 
 }
